f6571725be2c4c5448341e3bffc29414f7d7791e
Carolina Silva <carolsilva.acfs@gmail.com>
2020-11-30 23:18:21 +0000

    Add printing minimal parenteses

--- Main.hs
+++ Main.hs
@@ -9,14 +9,15 @@ stringOfTerm t = case t of
   K -> "K"
   I -> "I"
   Var x -> x
-  App t1 t2 -> "(" ++ stringOfTerm t1 ++ " " ++ stringOfTerm t2 ++ ")"
+  App t (App t1 t2) -> stringOfTerm t ++ " (" ++ stringOfTerm (App t1 t2) ++ ")"
+  App t1 t2 -> stringOfTerm t1 ++ " " ++ stringOfTerm t2
 
 eval :: Term -> Term
 eval t = case t of
   Grp t   -> eval t
-  App I x -> x
-  App (App K x) _ -> x
-  App (App (App S x) y) z -> App (App x z) (App y z)
+  App I x -> eval x
+  App (App K x) _ -> eval x
+  App (App (App S x) y) z -> eval (App (eval (App x z)) (eval (App y z)))
   App t1 t2 ->
     let t1' = eval t1
      in let t2' = eval t2
@@ -30,4 +31,5 @@ main = do
   s <- getContents
   let ast = parse (scanTokens s)
   print ast
+  print (eval ast)
   print (stringOfTerm (eval ast))
